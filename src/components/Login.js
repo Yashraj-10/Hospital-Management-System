ca89f49520a989564ef8df6d43a535af93add325
Yashraj Singh <yash10122001@gmail.com>
2023-03-03 04:58:48 +0530

    integrated login api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 const Login = () => {
+    const [user_id, setUser_id] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user_id, password);
+        // let ret_val = getLogin(e, user_id, password);
+        // console.log(123)
+        // console.log(ret_val)
+        // console.log(123)
+        // if(ret_val === 200) {
+        //     history.push("/frontdesk");
+        // }
+        // else {
+        //     history.push("/login");
+        // }
+
+        axios.post("/login", {user_id: user_id, password: password})
+        .then((response) => {
+            console.log(response);
+            history.push("/frontdesk");
+        }
+        , (error) => {
+            console.log(error);
+            history.push("/login");
+        }
+        )
+    }
     return (
         
         <div className='login-box'>
@@ -11,6 +44,7 @@ const Login = () => {
                         type="text"
                         name=""
                         required
+                        onChange={(e) => setUser_id(e.target.value)}
                     />
                     <label>Username</label>
                 </div>
@@ -19,16 +53,17 @@ const Login = () => {
                         type="password"
                         name=""
                         required
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                     <label>Password</label>
                 </div>
-                <Link to="#">
-                    {/* <span></span>
+                <button className='yashrajButton' onClick={handleSubmit}>
                     <span></span>
                     <span></span>
-                    <span></span> */}
+                    <span></span>
+                    <span></span>
                     Log In
-                </Link>
+                </button>
             </form>
         </div>
     );
