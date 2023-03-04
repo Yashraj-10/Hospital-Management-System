be1738943d412dbb3512734550ca0cd15556b921
vikasb9749 <vb8987850@gmail.com>
2023-03-05 01:38:56 +0530

    merged to backend

--- backend/.flaskenv
+++ backend/.flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=base.py
 FLASK_ENV=development
+FLASK_DEBUG=1
--- database_files/creatingTables.sql
+++ database_files/creatingTables.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS Users (
     name VARCHAR(100) NOT NULL,
     ph_number CHAR(12) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    type CHAR(1) NOT NULL,
+    type CHAR(3) NOT NULL,
+    access_token VARCHAR(1000),
+    access_token_expiry TIMESTAMP,
     address VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id)
 );
@@ -108,3 +110,42 @@ CREATE TABLE IF NOT EXISTS test_appointment (
 -- Demo
 
 INSERT INTO Users (user_id, name, ph_number, password, type, address) VALUES ('F1234', 'Vikas', '911234567890', 'asdfghjkl', 'F', 'Delhi');
+
+-- new insertions
+INSERT INTO Patients VALUES ('P1', 'Nirbhay', '2018-04-24 10:47', 'abc', 'gaya', 'bawasir');
+INSERT INTO Test VALUES ('T1', 'Bawasir Operation');
+INSERT INTO test_appointment(test_appointment_result_id, test_id, patient_id, start_time, end_time, test_status, report_link, result, comment) VALUES ('LK112', 'T1', 'P1', '2018-04-24 10:47','2018-04-24 10:53',NULL,NULL,NULL,NULL);
+
+
+
+-- new insertions
+INSERT INTO Patients VALUES ('P2', 'Astitva', '2018-04-24 10:47', 'abc', 'purnia', 'fever');
+INSERT INTO Users VALUES ('User2', 'Strange', '9112', 'strange', 'D', 'New york'); 
+INSERT INTO Users VALUES ('D1', 'Strange', '9112', 'strange', 'D', 'New york'); 
+INSERT INTO Doctors VALUES ('D1', 'OPD', 'Strange@','User2');
+INSERT INTO doc_appointment VALUES ('APP1', 'D1', 'P2', '2018-04-24 10:47', '2018-04-24 10:47', '1', 'Fever', '.');
+
+-- new insertions
+INSERT INTO doc_appointment VALUES ('APP2', 'D1', 'P1', '2018-04-24 10:50', '2018-04-24 11:32', '1', 'Bawasir', 'Cold water treatment');
+
+-- room insertions
+INSERT INTO Room VALUES ('B100', 'ICU');
+INSERT INTO Room VALUES ('B101', 'ICU');
+INSERT INTO Room VALUES ('B102', 'ICU');
+INSERT INTO Room VALUES ('B103', 'ICU');
+INSERT INTO Room VALUES ('C101', 'CCU');
+INSERT INTO Room VALUES ('C102', 'CCU');
+INSERT INTO Room VALUES ('C103', 'CCU');
+INSERT INTO Room VALUES ('C104', 'CCU');
+INSERT INTO Room VALUES ('A200', 'OT');
+INSERT INTO Room VALUES ('A201', 'OT');
+INSERT INTO Room VALUES ('C201', 'General Ward');
+INSERT INTO Room VALUES ('C202', 'General Ward');
+INSERT INTO Room VALUES ('C203', 'General Ward');
+INSERT INTO Room VALUES ('C204', 'General Ward');
+INSERT INTO Room VALUES ('A301', 'AC Ward');
+INSERT INTO Room VALUES ('A302', 'AC Ward');
+INSERT INTO Room VALUES ('A303', 'AC Ward');
+INSERT INTO Room VALUES ('A304', 'AC Ward');
+
+
