96d630ec59e569e6f0c93d44990865c430119643
vikasb9749 <vb8987850@gmail.com>
2023-03-08 00:00:40 +0530

    Adm Dshbrd table, delete user,cards,search working

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,18 +1,34 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import StickyHeadTable from './AdminTable';
 import '../styles/Admdb.css';
 
 import CheckboxesGroup from './AdminFilter';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const AdminDashboard = () => {
 
     const [searchInput, setSearchInput] = useState("");
 
+    const [post, setPost] = React.useState(null);
+    const [usertype, setUsertype] = React.useState(null);
     const history = useHistory();
     const handleChange = (e) => {
         e.preventDefault();
-        setSearchInput(e.target.value);
+        // setSearchInput(e.target.value);
+        console.log(searchInput);
+        axios
+            .post('https://dbms-backend-api.azurewebsites.net/user?search_string='.concat(`${searchInput}`), {
+                access_token: localStorage.getItem("access_token")
+            })
+            .then(
+                (response) => {
+                    setPost(response.data);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
     };
 
     const handleAddUser = (e) => {
@@ -21,76 +37,83 @@ const AdminDashboard = () => {
     };
 
     let data = [];
-    data.push({"DBA":15, "DEO": 20, "FDO": 30, "DOC": 40});
+    data.push({ "DBA": 15, "DEO": 20, "FDO": 30, "DOC": 40 });
 
-    let users = [];
-    users.push(
-    {
-        "address": "KGP",
-        "name": "Vikas Basti",
-        "ph_number": "8830475325  ",
-        "type": "doc",
-        "user_id": "DOC1"
-    });
-    users.push(
-    {
-        "address": "Delhi",
-        "name": "Yashraj",
-        "ph_number": "916393746715",
-        "type": "dba",
-        "user_id": "DBA1"
-    });
-    users.push(
-    {
-        "address": "Delhi",
-        "name": "Astitva",
-        "ph_number": "910987654321",
-        "type": "fdo",
-        "user_id": "FDO2"
-    });
 
-    return ( 
+
+    useEffect(() => {
+        var self_user_id = localStorage.getItem("self_user_id");
+        axios.post('https://dbms-backend-api.azurewebsites.net/users', {
+            access_token: localStorage.getItem("access_token")
+        })
+            .then(
+                (response) => {
+                    setPost(response.data);
+                    // console.log(response.data);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+
+        axios.post('https://dbms-backend-api.azurewebsites.net/user_type_count', {
+            access_token: localStorage.getItem("access_token")
+        })
+            .then(
+                (response) => {
+                    setUsertype(response.data);
+                    // console.log(response.data);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+
+    }, [])
+
+
+    return (
         <div>
             <div className="admind_header">
-                <input type="text" placeholder='Search here' onChange={handleChange} value={searchInput} className="searchTerm"></input>
-                <button type="submit" className="searchButton">Go</button>
+                <input type="text" placeholder='Search here' onChange={(e) => setSearchInput(e.target.value)} value={searchInput} className="searchTerm"></input>
+                <button type="submit" className="searchButton" onClick={handleChange}>Go</button>
                 <button className="aduser" onClick={handleAddUser}>Add User</button>
             </div>
             <div>
                 <div class="dropdown">
                     <CheckboxesGroup />
                 </div>
-                <div className="admind_table">
-                    <StickyHeadTable users={users}/>
-                </div>
-                <div className="usr_cards">
+                {post && <div className="admind_table">
+                    <StickyHeadTable users={post} />
+                </div>}
+                {usertype && <div className="usr_cards">
                     <div className="usr_types_dba">
                         <h3>DBAs</h3>
-                        <h2>{data[0]["DBA"]}</h2>
+                        <h2>{usertype.DBA}</h2>
                     </div>
                     <br />
                     <div className="usr_types_fd">
                         <h3>FDOs</h3>
-                        <h2>{data[0]["FDO"]}</h2>
+                        <h2>{usertype.FDO}</h2>
                     </div>
                     <br />
                     <div className="usr_types_doctor">
                         <h3>Doctors</h3>
-                        <h2>{data[0]["DOC"]}</h2>
+                        <h2>{usertype.DOC}</h2>
                     </div>
                     <br />
                     <div className="usr_types_de">
                         <h3>DEOs</h3>
-                        <h2>{data[0]["DEO"]}</h2>
+                        <h2>{usertype.DEO}</h2>
                     </div>
-                    <div className="admind_footer">
+                    {/* <div className="admind_footer">
                         <br />
                         <h3>Quantity</h3>
-                    </div>
-                </div>
+                    </div> */}
+                </div>}
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default AdminDashboard;
--- src/components/AdminTable.js
+++ src/components/AdminTable.js
@@ -10,7 +10,9 @@ import TableRow from '@mui/material/TableRow';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FormDialogReset from './ResetPasswordPopup';
-
+import { minWidth } from '@mui/system';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 //no IE 11 support
 
 const columns = [
@@ -30,38 +32,50 @@ const columns = [
     minWidth: 170
   },
   {
+    id: 'ph_number',
+    label: 'Contact No.',
+    minWidth: 100
+  },
+  {
     id: 'psswd',
     label: '',
   },
+  
   {
     id: 'del',
     label: '',
   }
 ];
 
-function createData(name, type, user_id) {
-  return { name, type, user_id};
+function createData(name, type, user_id, ph_number) {
+  return { name, type, user_id, ph_number};
 }
 
-// const rows = [
-//   createData('Nirbhay', 'DBA', 'jnv_45'),
-//   createData('Pranil', 'Dr', 'puchhi_18'),
-//   createData('Vikas', 'FD', 'basti_7'),
-//   createData('Nirbhay', 'DBA', 'jnv_45'),
-//   createData('Pranil', 'Dr', 'puchhi_18'),
-//   createData('Vikas', 'FD', 'basti_7'),
-//   createData('Nirbhay', 'DBA', 'jnv_45'),
-//   createData('Pranil', 'Dr', 'puchhi_18'),
-//   createData('Vikas', 'FD', 'basti_7'),
-//   createData('Nirbhay', 'DBA', 'jnv_45'),
-//   createData('Pranil', 'Dr', 'puchhi_18'),
-//   createData('Vikas', 'FD', 'basti_7'),
-// ];
 
 export default function StickyHeadTable(props) {
   const rows = props.users;
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleDelete = (e, userID) => {
+    console.log('delete called');
+    axios.post('https://dbms-backend-api.azurewebsites.net/users/delete', {
+              access_token: localStorage.getItem("access_token"),
+              user_id: userID
+          })
+              .then(
+                  (response) => {
+                      // setPost(response.data);
+                      console.log(response.data);
+                      alert(response.data.message);
+                  },
+                  (error) => {
+                      console.log(error);
+                      alert(error.response.data.message);
+                  }
+              );
+    console.log(userID);
+  }
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -71,7 +85,7 @@ export default function StickyHeadTable(props) {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-
+  
   return (
     <Paper sx={{ width: '90%', overflow: 'hidden' }}>
       <TableContainer sx={{ maxHeight: 550 }}>
@@ -100,7 +114,7 @@ export default function StickyHeadTable(props) {
                         <TableCell key={column.id} align={column.align}>
                           {
                             column.id === 'psswd' ? <FormDialogReset /> :
-                            column.id === 'del' ? <IconButton aria-label="delete"><DeleteIcon /></IconButton> :
+                            column.id === 'del' ? <IconButton aria-label="delete"><DeleteIcon onClick= {(e) => handleDelete(e,row.user_id)}/></IconButton> :
                             column.format && typeof value === 'number'
                             ? column.format(value)
                             : value
